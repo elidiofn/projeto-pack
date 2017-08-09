bd6b93790ee8f314db6bb7d2ebc3a77f15de57ca
joaoalcimar <joao.alcimar.junior@gmail.com>
2017-08-09 02:01:15 -0300

    Novo tipo de queixa: animal

---
+++ src/main/java/com/ufcg/si1/model/QueixaAnimal.java
@@ -0,0 +1,15 @@
+package com.ufcg.si1.model;
+
+public class QueixaAnimal extends Queixa{
+
+	private String tipoDoAnimal;
+	
+	public String getTipoDoAnimal() {
+		return tipoDoAnimal;
+	}
+
+	public void setTipoDoAnimal(String tipoDoAnimal) {
+		this.tipoDoAnimal = tipoDoAnimal;
+	}
+	
+}
